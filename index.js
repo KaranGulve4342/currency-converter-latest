a265151d6e44f9497725bde6377fd4e68b494fd8
KaranGulve4342 <karangulve4342@gmail.com>
2024-03-02 11:11:46 +0530

    added comments

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+import Freecurrencyapi from '@everapi/freecurrencyapi-js'
 
+// Initialize the Freecurrencyapi object with the provided API key
 const freecurrencyapi = new Freecurrencyapi('fca_live_dt8DATfoeKOXtBPD2JAiy7Tp0cqNedSRiz77fizs');
 
-
+/**
+* Convert a specified amount of money from one currency to another
+* @param {string} fromCurrency - The currency to convert from
+* @param {string} toCurrency - The currency to convert to
+* @param {number} units - The amount of money to convert
+* @returns {string} The converted amount, rounded to 10 decimal places
+*/
 export async function convertCurrency(fromCurrency, toCurrency, units) {
-    const res = await freecurrencyapi.latest({
-        base_currency: fromCurrency,
-        currencies: toCurrency
-    });
+   // Get the latest exchange rates from Freecurrencyapi
+   const res = await freecurrencyapi.latest({
+       base_currency: fromCurrency, // Set the base currency to the 'from' currency
+       currencies: toCurrency // Set the currencies to convert to the 'to' currency
+   });
 
-    const multiplier = res.data[toCurrency];
-    // console.log(multiplier);
-    return  (units * multiplier).toFixed(10);
+   // Get the exchange rate multiplier for the 'to' currency
+   const multiplier = res.data[toCurrency];
+
+   // Multiply the number of units by the multiplier to convert the currency
+   return (units * multiplier).toFixed(10);
 }
-
-
